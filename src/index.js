ecec5bf39e29821405e27bcdd3e5b827c30be272
pushpak <pushpak94681@gmail.com>
2026-06-22 13:04:36 +0530

    feat: add authentication middleware for protected routes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ const cors = require('cors');
 require('dotenv').config();
 
 const connectMongo = require('./config/mongoConfig');
-const prisma = require('./config/sqlconfig');
+const prisma = require('./config/sqlconfig')
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes); // every auth routes /api/auth se start honge
+
+//basic test routh
 app.get('/', (req,res) => {
     res.send('PulseTrack AI Backend is Runnig!');
+});
+
+//Protected routh test for logged inusers
+app.get('/api/protected', require('./middleware/authMiddleware').protect, (req,res) => {
+    res.json({
+        message: 'Ypu assessed a protcted route!',
+        user: req.user // middleware attach user
+    });
 });
 
 const PORT = process.env.PORT || 5000;
