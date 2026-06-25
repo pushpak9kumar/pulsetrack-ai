b33f2d976bb49998a632cb81acfde6b4ac2f0768
pushpak <pushpak94681@gmail.com>
2026-06-25 08:56:55 +0530

    feat: add AuthContext for global user state management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,61 @@
+//Context banao(global state container)
+const AuthContext = createContext();
+
+//Provider component (sabko state provide karega)
+export const AuthProvider = ({ children }) => {
+    //user state(nul  = not logged in, object = logged in)
+    const [user, setUser] = useState(null);
+    const[token, setToken] = useState(localStorage.getItem('token') || '');
+    const[loading, setLoading] = useState(true);
+
+    //App start hone pe check karo ki token localStorage me hai ya nahi 
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        const storedUser = localStorage.getItem('user');
+
+        if(storedToken && storedUser) {
+            setToken(storedToken);
+            setUser(JSON.parse(storedUser));
+        }
+        setLoading(false); //LOading khatam
+    }, []);
+
+    //Login function(backend se token aaya toh call hoga)
+    const login = (userData, tokenValue) => {
+        setUser(useData);
+        setToken(tokenValue);
+        localStorage.setItem('token', tokenValue);
+        localStorage.setItem('user', JSON.stringify(userData));
+    };
+
+    //Logout function
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        setToken('');
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+    };
+
+    //Sab values provide karo children ko
+    return (
+        <AuthContext.Provider value={{
+            user,
+            token,login,
+            logout,
+            loading,
+            isAuthenticated: !!user //Boolean - logged in or not
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+//Custom hook (for easy access)
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if(!context) {
+        throw new Error('useAuth must be used within AuthProvider');
+    }
+    return context;
+};
